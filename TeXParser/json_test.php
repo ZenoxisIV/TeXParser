0d20b1caf0ba15f78654d3674abd2d0064076c1f
Ivan Cadiang <iacadiang@up.edu.ph>
2024-02-23 12:00:30 +0800

    fix: added tildes to prevent issues with table naming

--- TeXParser/json_test.php
+++ TeXParser/json_test.php
@@ -11,11 +11,10 @@ catch(mysqli_sql_exception){
 	echo"Could not connect!";
 }
 
-//$tables = ['cd_year', 'cd_usage', 'servers', 'pc_os', 'ws_os', 'server_os', 'automation_software', 'admin_systems', 'si_systems'];
-$tables = ['databases'];
+$tables = ['cd_year', 'cd_usage', 'servers', 'pc_os', 'ws_os', 'server_os', 'automation_software', 'admin_systems', 'si_systems', 'databases'];
 
 foreach ($tables as $table) {
-    $sql = "SELECT * FROM $table WHERE Code = '991901'";
+    $sql = "SELECT * FROM `$table` WHERE Code = '991901'";
     $result = $connect->query($sql);
 
     if ($result->num_rows > 0) {
