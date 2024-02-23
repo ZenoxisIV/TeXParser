77efec0544de197a8b07e953fcd8909521652e76
Ivan Cadiang <iacadiang@up.edu.ph>
2024-02-23 11:00:58 +0800

    refactor: added JSON error catching and modified data handling

--- TeXParser/utilities/json_extract.py
+++ TeXParser/utilities/json_extract.py
@@ -2,7 +2,10 @@ from collections import defaultdict
 import requests
 
 def requestJSONData(url: str) -> dict:
-    return requests.get(url).json()
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise ConnectionError(f"Failed to retrieve JSON data from '{url}'. Status code: {response.status_code}")
+    return response.json()
 
 def parseJSONData(data: dict, table: str) -> defaultdict:
     if table not in data:
@@ -11,10 +14,15 @@ def parseJSONData(data: dict, table: str) -> defaultdict:
     row_data = data[table]['rows']
     column_data = data[table]['columns']
 
-    parsed_data = defaultdict(lambda: None)
-    parsed_data['row_names'] = tuple(column_data[1:])
-    parsed_data['row_entries'] = [tuple(row[1:]) for row in row_data]
+    parsed_data: defaultdict | None = defaultdict(lambda: None)
+
+    # Skip the row entries in column 'Code'
+    parsed_data['col_names'] = column_data[1:]
+    parsed_data['row_entries'] = [row[1:] for row in row_data]
+
     parsed_data['col_len'] = len(column_data) - 1
-    parsed_data['row_len'] = len(row_data) - 1
+    parsed_data['row_len'] = len(row_data)
 
     return parsed_data
+
+print(requestJSONData("http://localhost/TeXParser/json_test.php"))
