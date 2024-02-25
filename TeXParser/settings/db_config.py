a1d472ff061a206fb320a163b6b620f6b2fe886a
Ivan Cadiang <iacadiang@up.edu.ph>
2024-02-25 15:49:11 +0800

    refactor: distributed config files

---
+++ TeXParser/settings/db_config.py
@@ -0,0 +1,29 @@
+# ======================================
+# DATABASE-RELATED CONFIGURATIONS
+# ======================================
+FORM_NAMES = {
+    "1.1": "cd_year",
+    "1.2": "cd_usage",
+    "1.3": "servers",
+    "2.1.1": "pc_os",
+    "2.1.2": "ws_os",
+    "2.1.3": "server_os",
+    "2.2": "automation_software",
+    "2.3": "admin_systems",
+    "2.4": "si_systems",
+    "2.5": "databases",
+}
+
+TICKBOX_COLS = {
+    "1.1": [],
+    "1.2": [],
+    "1.3": [],
+    "2.1.1": ["Lifetime"],
+    "2.1.2": ["Lifetime"],
+    "2.1.3": ["Lifetime"],
+    "2.2": ["Lifetime"],
+    "2.3": [],
+    "2.4": [],
+    "2.5": [],
+}
+# ======================================
---
+++ TeXParser/settings/footer_config.py
@@ -0,0 +1,63 @@
+# ======================================
+# FOOTER CONFIGURATIONS
+# ======================================
+
+# ===================================================================================================================
+# GUIDELINES:
+# If you want to add bold text, enclose the text with \textbf{<text>}.
+# If you want to add italicized text, enclose the text with \textit{<text>}.
+# If ypu want to add underline text, enclose the text with \underline{<text>}.
+# If you want to add a hyperlink, enclose the text with \href{<link>}{<text>}.
+# If you want to add a footnote mark, use \footnotemark[<symbol>].
+# Lastly, make sure to add a 'r' just right before you write inside the "" to avoid escape characters issues!
+# ===================================================================================================================
+FOOTNOTES = {
+    "1": "In case all three positions are occupied by different persons, then the IS Planner should have priority in answering this survey.",
+    "2": "Count only the mobile phones owned or leased by your agency.",
+    "3": "Those used in planning, coordination, internal training, monitoring and evaluation",
+    "4": "Those used by external clients",
+    "5": r"Mark if yes. Examples are OEM license (software is already installed in the hardware) and Enterprise (Perpetual) license, which does not require renewal and is for life long. (source: \href{http://www.manageengine.com/products/service-desk/help/adminguide/configurations/software/software-license-type.html}{http://www.manageengine.com/products/service-desk/help/adminguide/configurations/software/software-license-type.html})",
+    "6": "Include only those currently being used by your office or agency.",
+    "7": "Payroll System, 201 File Information and Promotion System, Vehicle Monitoring System, Document Tracking System, Attendance and Leave Monitoring System, Financial Management Information System, Inventory System, Records Management System",
+    "8": "Write Y for Yes if your agency has intellectual property right to the system. Write N for No.",
+    "9": r"\underline{WORKING ENVIRONMENT}: \textbf{S} - Stand alone; \textbf{C} - Client-Server; \textbf{W} - Web-based",
+    "10": r"\underline{USE}: \textbf{1} - Public Financial Management; \textbf{2} - Citizen Frontline Services; \textbf{3} - Ease of Doing Business; \textbf{4} - Higher Education; \textbf{5} - Basic Education; \textbf{6} - Health; \textbf{7} - Justice, Peace and Order; \textbf{8} - Energy; \textbf{9} - Land and Other Geospatial Information; \textbf{10} - Disaster and Climate Change Management; \textbf{11} - Public Works and Transport; \textbf{12} - iGov and ICT Infrastructure; \textbf{13} - Transparency and Citizen's Participation; \textbf{14} - Citizen Registry; \textbf{15} - Others, please specify.",
+    "11": "Include only those currently being used by your office or agency.",
+    "12": r"eCensus, Electronic Filing and Payment System, eTIN, Government e-Procurement System, Automated Customs Operations System, Electronic Customs Clearance Facility, Licensure Examination \& Registration Integrated System, Machine Readable Passports and Visas, Philippine Land Registration and Information System, Government Employees Management Information System, e-GSIS, eReal Property Tax System, Business Permit \& License System, iRegister, Hospital Operations and Management Information System",
+    "13": "Write Y for Yes if your agency has intellectual property right to the database. Write N for No.",
+    "14": r"\underline{WORKING ENVIRONMENT}: \textbf{S} - Stand alone; \textbf{C} - Client-Server; \textbf{W} - Web-based",
+    "15": r"\underline{USE}: \textbf{1} - Public Financial Management; \textbf{2} - Citizen Frontline Services; \textbf{3} - Ease of Doing Business; \textbf{4} - Higher Education; \textbf{5} - Basic Education; \textbf{6} - Health; \textbf{7} - Justice, Peace and Order; \textbf{8} - Energy; \textbf{9} - Land and Other Geospatial Information; \textbf{10} - Disaster and Climate Change Management; \textbf{11} - Public Works and Transport; \textbf{12} - iGov and ICT Infrastructure; \textbf{13} - Transparency and Citizen's Participation; \textbf{14} - Citizen Registry; \textbf{15} - Others, please specify.",
+    "16": "Briefly describe the purpose or importance of the database.",
+    "17": "Examples of DBMS are MS Excel, MS Access, MS SQL Server, MySQL, IBM's DB2, Oracle SQL, Sybase SQL, Informix, FoxPro",
+    "18": r"\underline{USE}: \textbf{1} - Public Financial Management; \textbf{2} - Citizen Frontline Services; \textbf{3} - Ease of Doing Business; \textbf{4} - Higher Education; \textbf{5} - Basic Education; \textbf{6} - Health; \textbf{7} - Justice, Peace and Order; \textbf{8} - Energy; \textbf{9} - Land and Other Geospatial Information; \textbf{10} - Disaster and Climate Change Management; \textbf{11} - Public Works and Transport; \textbf{12} - iGov and ICT Infrastructure; \textbf{13} - Transparency and Citizen's Participation; \textbf{14} - Citizen Registry; \textbf{15} - Others, please specify.",
+    "19": r"\underline{PROJECT NAME}: In case an ICT project is divided in phases and its budget is given by phases, kindly list each phase as a separate project tagged as $<$Project Name$>$ Ph. 1, $<$Project Name$>$ Ph. 2, and so on.",
+    "20": r"\underline{COST}: For ICT projects and project phases that \underline{ended in 2013 or earlier}, kindly provide the \textbf{\underline{actual}} \textbf{\underline{cost}} in pesos and not the proposed cost.",
+    "21": r"\underline{DEVELOPMENT STRATEGY}: \textbf{I} - In-house;  \textbf{O} - Outsourced;  \textbf{C} - Combination",
+    "22": r"\underline{STATUS}: \textbf{U} - Under Development;  \textbf{D} - For Deployment; \textbf{O} - Operational",
+    "23": r"\underline{USE}: \textbf{1} - Public Financial Management; \textbf{2} - Citizen Frontline Services; \textbf{3} - Ease of Doing Business; \textbf{4} - Higher Education; \textbf{5} - Basic Education; \textbf{6} - Health; \textbf{7} - Justice, Peace and Order; \textbf{8} - Energy; \textbf{9} - Land and Other Geospatial Information; \textbf{10} - Disaster and Climate Change Management; \textbf{11} - Public Works and Transport; \textbf{12} - iGov and ICT Infrastructure; \textbf{13} - Transparency and Citizen's Participation; \textbf{14} - Citizen Registry; \textbf{15} - Others, please specify.",
+    "24": "ISSP Template Revised 2003 iib",
+    "25": r"\href{http://searchsecurity.techtarget.com/definition/Automated-Fingerprint-Identification-System}{http://searchsecurity.techtarget.com/definition/Automated-Fingerprint-Identification-System}",
+    "26": r"\href{http://en.wikipedia.org/wiki/Cloud_computing}{http://en.wikipedia.org/wiki/Cloud\_computing}",
+    "27": r"\href{http://searchsoa.techtarget.com/definition/collocation}{http://searchsoa.techtarget.com/definition/collocation}",
+    "28": r"\href{http://searchdatacenter.techtarget.com/definition/data-center}{http://searchdatacenter.techtarget.com/definition/data-center}",
+    "29": r"\href{http://computer.howstuffworks.com/10-types-of-computers.htm#page=2}{http://computer.howstuffworks.com/10-types-of-computers.htm#page=2}",
+    "30": r"\href{http://linux.about.com/cs/linux101/g/digital_signatu.htm?terms=Digital+signature}{http://linux.about.com/cs/linux101/g/digital\_signatu.htm?terms=Digital+signature}",
+    "31": r"\href{http://www.wisegeek.com/what-is-an-external-hard-drive.htm}{http://www.wisegeek.com/what-is-an-external-hard-drive.htm}",
+    "32": r"\href{http://www.nwgis.com/gisdefn.htm}{http://www.nwgis.com/gisdefn.htm}",
+    "33": "",
+    "34": "",
+    "35": "",
+    "36": "",
+    "37": "",
+    "38": "",
+    "39": "",
+    "40": "",
+    "41": "",
+    "42": "",
+    "43": "",
+    "44": "",
+    "45": "",
+    "46": "",
+    "47": "",
+    "48": "",
+}
